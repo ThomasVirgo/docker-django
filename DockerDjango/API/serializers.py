4cd53ee839070568ebb03e092ce3afa30fe388dc
Tom Virgo <tomcvirgo@gmail.com>
2021-09-01 19:18:38 +0100

    add user registration

--- DockerDjango/API/serializers.py
+++ DockerDjango/API/serializers.py
@@ -5,3 +5,27 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    password_confirmation = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'password_confirmation')
+        write_only_fields = ('password', 'password_confirmation')
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username']
+        )
+        password=validated_data['password']
+        password_confirmation=validated_data['password_confirmation']
+
+        if password != password_confirmation:
+            raise serializers.ValidationError({'password': 'Passwords must match.'}) 
+        user.set_password(password)
+        user.save()
+
+        return user   
--- DockerDjango/API/views.py
+++ DockerDjango/API/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
+from .serializers import UserRegistrationSerializer
 
 # Create your views here.
 class DemoView(APIView):
@@ -9,3 +11,11 @@ class DemoView(APIView):
     def get(self, request):
         content = {'message': 'Hello! This is a Demo!'}
         return Response(content)
+
+class UserRegistrationView(APIView):
+    def post(self, request, format=None):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
