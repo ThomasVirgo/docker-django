9e9df793b1bee6b928baad8cc7998224aaff94a1
Tom Virgo <tomcvirgo@gmail.com>
2021-09-01 18:35:15 +0100

    add token auth settings

--- DockerDjango/API/views.py
+++ DockerDjango/API/views.py
@@ -1,8 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class DemoView(APIView):
+    permission_classes = (IsAuthenticated,)
+
     def get(self, request):
         content = {'message': 'Hello! This is a Demo!'}
         return Response(content)
